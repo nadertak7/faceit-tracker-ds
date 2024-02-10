fc3c26b39bfa6cbf69f098a9ca8525db0305ecd2
Nader Tak <120279049+nadertak7@users.noreply.github.com>
2024-02-10 21:26:30 +0000

    Add statistics to the summary page

--- 1_Summary.py
+++ 1_Summary.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from utilities import spacing
+from utilities import pageelements
+from utilities.faceitstatisticsretrieval import RetrieveFaceitData
 
 # Set config
 st.set_page_config(page_title = "Email Sender", layout = "wide")
@@ -9,5 +10,16 @@ with st.container():
     left_column, middle_column, right_column = st.columns([0.2, 0.4, 0.4])
     with middle_column:
         st.header("Faceit Stats DS")
-        spacing.large_vertical_space(5)
-        st.text_input("Enter Faceit Nickname:")
+        pageelements.large_vertical_space(1)
+        user_input = st.text_input("Enter Faceit Nickname:")
+        pageelements.small_vertical_space(1)
+
+        if user_input:
+            faceit_data = RetrieveFaceitData(user_input)
+            faceit_data.request_data()
+            player_statistics = faceit_data.retrieve_statistics()
+
+            left_metric_column, right_metric_column = st.columns(2)
+            left_metric_column.metric("Elo", player_statistics.get("elo"))
+            right_metric_column.metric("Faceit Level", player_statistics.get("skill-level"))
+
---
+++ utilities/faceitstatisticsretrieval.py
@@ -0,0 +1,82 @@
+# Import modules
+import os
+from dotenv import load_dotenv
+import requests
+import streamlit as st
+
+class RetrieveFaceitData:
+    """
+    A class to retrieve Faceit data for a given player.
+
+    Attributes:
+        faceit_name (str): The Faceit username for which data will be retrieved.
+        response_api (requests.Response): The response object obtained from the Faceit API.
+    """
+    # Load secrets
+    load_dotenv()
+ 
+    # Initialise instance variables  
+    def __init__(self, faceit_name):
+        """
+        Initialise the RetrieveFaceitData object.
+
+        Parameters:
+            faceit_name (str): The Faceit username for which data will be retrieved.
+        """
+        self.faceit_name = faceit_name
+
+    def request_data(self):
+        """
+        Send a GET request to the Faceit API to retrieve player data.
+
+        Raises:
+            Exception: If the player is not found or an error occurs during the request.
+        """
+        # Set GET request args
+        headers = {
+            # The SERVER_KEY environment variable was generated at:
+            # https://developers.faceit.com/apps > select app > api keys > create server side api key
+            "Authorization": f"Bearer {os.getenv("SERVER_KEY")}",
+            "accept": "application/json"
+        }
+
+        endpoint = f"https://open.faceit.com/data/v4/players?nickname={self.faceit_name}"
+
+        # GET request
+        try:
+            response_api = requests.get(
+                endpoint, 
+                headers=headers, 
+                timeout=100
+            )
+            # Error handling
+            if response_api.status_code == 200:
+                st.toast("Success")
+                self.response_api = response_api
+            elif response_api.status_code == 404:
+                raise Exception("Player was not found. Please ensure the nickname entered is correct.")
+            else:
+                raise Exception("Something went wrong.")
+      
+        except Exception as exception:
+            st.error(f"An error occurred: {exception}")
+
+    def retrieve_statistics(self):
+        """
+        Retrieve statistics from the response obtained from the Faceit API.
+
+        Returns:
+            dict: A dictionary containing player statistics.
+        """
+        player_summary = self.response_api.json()
+
+        cs2_player_data = (player_summary
+                   .get('games', {})
+                   .get('cs2', {})
+        )
+
+        player_statistics = {
+            "skill-level": cs2_player_data.get('skill_level', {}),
+            "elo": cs2_player_data.get('faceit_elo', {})
+        }
+        return player_statistics
