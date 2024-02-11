dbc235a55fc1989ea3135e65f676c312fa48031b
Nader Tak <120279049+nadertak7@users.noreply.github.com>
2024-02-11 00:42:23 +0000

    Add missing docstring

--- utilities/pageelements.py
+++ utilities/pageelements.py
@@ -8,9 +8,18 @@ def small_vertical_space(multiplier=1):
         multiplier (int): The number of newlines to be generated. Default is 1.
 
     Returns:
-        list: Streamlit code that runs 
+        list: Streamlit code
     """
     return [st.write('\n') for _ in range(multiplier)]
 
 def large_vertical_space(multiplier=1):
+    """
+    Generate a small vertical space in the Streamlit app by writing '#' with streamlit. 
+
+    Parameters:
+        multiplier (int): The number of vertical spaces to be generated. Default is 1.
+
+    Returns:
+        list: Streamlit code
+    """
     return [st.write('#') for _ in range(multiplier)]
