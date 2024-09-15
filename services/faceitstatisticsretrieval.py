9ccec15a694f68268148d6f5785d62b86f8ea293
Nader Tak <nadertak7@gmail.com>
2024-09-15 22:33:01 +0100

    Add comments, typehints and lint

--- services/faceitstatisticsretrieval.py
+++ services/faceitstatisticsretrieval.py
@@ -6,24 +6,24 @@ from dotenv import load_dotenv
 from glom import glom
 import requests
 import streamlit as st
-import sys
 
+# Allow access to utilities folder
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utilities.faceitstatisticscalculations import calculate_stats
 
 @dataclass
 class FaceitEndpoints:
+    """API endpoints that retrieve relevant player information"""
     player_info: str = "https://open.faceit.com/data/v4/players?nickname={nickname}"
     player_bans: str = "https://open.faceit.com/data/v4/players/{player_id}/bans"
-    player_statistics: str = "https://open.faceit.com/data/v4/players/{player_id}/games/{game_id}/stats?offset={offset}&limit={limit}"
-
-@dataclass
-class ApiArguments:
-    headers: Dict[str, str]
-    endpoint: str
+    player_statistics: str = """
+        https://open.faceit.com/data/v4/players/{player_id}/games/{game_id}/stats?offset={offset}&limit={limit}
+    """
 
 @dataclass
 class PlayerInformationData:
+    """A store for information about the player"""
     player_id: str
     nickname: str
     avatar: str
@@ -31,9 +31,11 @@ class PlayerInformationData:
     is_verified: bool
     skill_level: int
     elo: int
+    num_games: int
 
 @dataclass
 class PlayerBanInformationData:
+    """A store for informaiton about the player's bans"""
     player_id: str
     is_banned: bool
     is_smurf: bool
@@ -42,6 +44,7 @@ class PlayerBanInformationData:
 
 @dataclass
 class PlayerStatisticsAllTimeData:
+    """A store for the player's performance in Faceit across all their games"""
     player_id: str
     avg_kills: float
     avg_kr_ratio: float
@@ -56,6 +59,7 @@ class PlayerStatisticsAllTimeData:
 
 @dataclass
 class PlayerStatisticsLast20Data:
+    """A store for the player's performance in Faceit in their last 20 games"""
     player_id: str
     avg_kills: float
     avg_kr_ratio: float
@@ -70,6 +74,7 @@ class PlayerStatisticsLast20Data:
 
 @dataclass
 class PlayerStatisticsFirst10Data:
+    """A store for the player's performance in Faceit in their first 10 games"""
     player_id: str
     avg_kills: float
     avg_kr_ratio: float
@@ -83,12 +88,18 @@ class PlayerStatisticsFirst10Data:
     avg_score_diff: float
 
 class FaceitDataRetrieval:
+    """Handles the retrieval of a player's Faceit data"""
     def __init__(
         self,
-        faceit_nickname
+        faceit_nickname: str
     ) -> None:
+        """Initialises the class and its attributes
+
+        Args:
+            faceit_nickname (str): The nickname of the player on faceit
+        """
         self.faceit_nickname = faceit_nickname
-        self.headers = self._initialise_api()
+        self.headers = self._initialise_api_header()
         self.player_data = (self
             ._request_data(FaceitEndpoints.player_info
                 .format(nickname=self.faceit_nickname)
@@ -105,7 +116,15 @@ class FaceitDataRetrieval:
         self.all_cs_game_stats = self.player_cs2_game_stats + self.player_csgo_game_stats
 
     @staticmethod
-    def _initialise_api() -> Dict[str, str]:
+    def _initialise_api_header() -> Dict[str, str]:
+        """Loads the header that will be used for each request
+
+        Raises:
+            KeyError: If the SERVER_KEY environment variable does not exist
+
+        Returns:
+            Dict[str, str]: Headers for the get request
+        """
         # Handle server key
         load_dotenv()
         # The SERVER_KEY environment variable was generated at:
@@ -122,15 +141,25 @@ class FaceitDataRetrieval:
 
     def _request_data(
             self,
-            endpoint
+            endpoint: str
     ) -> requests.Response:
+        """Sends a request to the provided API endpoint, handling errors in the process
+
+        Args:
+            endpoint (str): The endpoint URL
+
+        Returns:
+            requests.Response: The response of the get request
+        """
         try:
             response_api = requests.get(
+                # strip method is needed due to formatting of multi-line strings
                 endpoint.strip(),
                 headers=self.headers,
                 timeout=20
             )
             response_api.raise_for_status()
+        # Handle errors and present to user on front-end
         except requests.exceptions.HTTPError as http_error:
             st.error(f"Http Error: {http_error}")
         except requests.exceptions.ConnectionError as conn_error:
@@ -142,6 +171,7 @@ class FaceitDataRetrieval:
         return response_api.json()
 
     def player_data_store(self) -> PlayerInformationData:
+        """Inserts the player's information into the PlayerInformationData dataclass"""
         return PlayerInformationData(
             player_id=self.player_data.get("player_id"),
             nickname=self.player_data.get("nickname"),
@@ -149,10 +179,12 @@ class FaceitDataRetrieval:
             num_friends=len(self.player_data.get("friends_ids", [])),
             is_verified=self.player_data.get("verified"),
             skill_level=glom(self.player_data, "games.cs2.skill_level", default=0),
-            elo=glom(self.player_data, "games.cs2.faceit_elo", default=0)
+            elo=glom(self.player_data, "games.cs2.faceit_elo", default=0),
+            num_games=len(self.player_cs2_game_stats) + len(self.player_csgo_game_stats)
         )
 
     def player_data_ban_store(self) -> PlayerBanInformationData:
+        """Inserts the player's ban information into the PlayerBanInformationData dataclass"""
         player_ban_items = self.player_ban_data.get("items", [])
         return PlayerBanInformationData(
             player_id = self.player_id,
@@ -161,17 +193,19 @@ class FaceitDataRetrieval:
             num_bans=len(player_ban_items),
             ban_response=player_ban_items
         )
-    
 
-    def fetch_cs_game_stats(self, game_id):
+    def fetch_cs_game_stats(self, game_id: str) -> Dict:
+        """Handles paginated stats data"""
         match_stats = []
+        # Endpoint fetches results {limit} at a time
+        # And adds to offset until end is reached
         offset = 0
         limit = 100
         while True:
             endpoint = FaceitEndpoints.player_statistics.format(
                 player_id=self.player_id,
                 game_id=game_id,
-                offset=offset, 
+                offset=offset,
                 limit=limit
             )
             player_stats_batch = self._request_data(endpoint)
@@ -182,16 +216,20 @@ class FaceitDataRetrieval:
             offset += limit
         return match_stats
 
-    def player_data_stats_all_time_store(self):
-        print(calculate_stats(self.all_cs_game_stats))
+    def player_data_stats_all_time_store(self) -> PlayerStatisticsAllTimeData:
+        """Inserts the player's all time stats into the relevant dataclass"""
         return PlayerStatisticsAllTimeData(self.player_id, **calculate_stats(self.all_cs_game_stats))
-    
-    def player_data_stats_last_20_store(self):
+
+    def player_data_stats_last_20_store(self) -> PlayerStatisticsLast20Data:
+        """Inserts the player's last 20 game stats into the relevant dataclass"""
+        # Extract first 20 records in all_cs_game_stats
         last_20_cs_game_stats = self.all_cs_game_stats[:20]
         return PlayerStatisticsLast20Data(self.player_id, **calculate_stats(last_20_cs_game_stats))
-    
+
     def player_data_stats_first_10_store(self):
+        # Extract last 10 records in all_cs_game_stats
+        """Inserts the player's first 10 game stats into the relevant dataclass"""
         first_10_cs_game_stats = self.all_cs_game_stats[-len(self.all_cs_game_stats):-len(self.all_cs_game_stats) + 10]
         return PlayerStatisticsFirst10Data(self.player_id, **calculate_stats(first_10_cs_game_stats))
 
-print(FaceitDataRetrieval("NadseN").player_data_stats_all_time_store())
+print(FaceitDataRetrieval("NadseN").player_data_store())
--- utilities/faceitstatisticscalculations.py
+++ utilities/faceitstatisticscalculations.py
@@ -1,31 +1,63 @@
+from typing import List, Dict
+
 import numpy as np
 
-def _calculate_winrate(match_dictionary: dict) -> float:
+def _calculate_winrate(match_dictionary: List[Dict]) -> float:
+    """Caluclates a player's winrate from a list of boolean match results
+
+    Args:
+        match_dictionary (List[Dict]): The match data to calculate from
+
+    Returns:
+        float: The player's winrate
+    """
     if not match_dictionary:
         return 0.00
     results = [int(match.get("Result", 0)) for match in match_dictionary]
     results_array = np.array(results)
+    # Calculates percentage of 1 values in the array of player results
     win_percentage = (
         (np.sum(results_array == 1) / len(results_array)) * 100
     )
     return round(float(win_percentage), 2)
 
-def _calculate_point_difference(match_dictionary: dict) -> int:
+def _calculate_point_difference(match_dictionary: List[Dict]) -> int:
+    """Calculates the average point difference from a list of boolean match
+    results and scores
+
+    Args:
+        match_dictionary (List[Dict]): The match data to calculate from
+
+    Returns:
+        int: The player's average point difference
+    """
     if not match_dictionary:
         return 0.00
     results = [int(match.get("Result", 0)) for match in match_dictionary]
     game_scores = [match.get("Score", 0) for match in match_dictionary]
-    
+
+    # Splits strings in formats "n1 / n2"
     split_score_array = np.array([x.split(' / ') for x in game_scores], dtype=int)
     results_array = np.array(results, dtype=int)
-    
+
+    # Calculates absolute score difference
     scores_diff_absolute = np.abs(split_score_array[:, 0] - split_score_array[:, 1])
+    # Returns a negative score difference for a loss and positive for a win
     scores_diff_adjusted = np.where(results_array == 1, scores_diff_absolute, -scores_diff_absolute)
     scores_diff_adjusted_average = np.mean(scores_diff_adjusted)
     print(round(float(scores_diff_adjusted_average), 2))
     return round(float(scores_diff_adjusted_average), 2)
 
-def calculate_stats(match_dictionary: dict) -> dict:
+def calculate_stats(match_dictionary: List[Dict]) -> Dict[str, float]:
+    """Calculates a number of metrics that already exist in the endpoint.
+    Then adds the hidden methods above to the same data structure
+
+    Args:
+        match_dictionary (List[Dict]): The match data to calculate from
+
+    Returns:
+        Dict[str, float]: A dictionary of the player's various calculated statistics
+    """
     key_mapping = {
         "Kills": "avg_kills",
         "K/R Ratio": "avg_kr_ratio",
@@ -43,6 +75,7 @@ def calculate_stats(match_dictionary: dict) -> dict:
     if not match_dictionary:
         return {value: 0.00 for value in key_mapping.values()}
 
+    # Looks at the endpoint response for most items in the key_mapping dict
     averages = {}
     for key, new_key in key_mapping.items():
         results = [float(match.get(key, 0)) for match in match_dictionary]
@@ -50,6 +83,7 @@ def calculate_stats(match_dictionary: dict) -> dict:
         results_array_average = np.mean(results_array)
         averages[new_key] = round(float(results_array_average), 2)
 
+    # Adds the custom calculations from the above.
     averages["perc_winrate"] = _calculate_winrate(match_dictionary)
     averages["avg_score_diff"] = _calculate_point_difference(match_dictionary)
 
